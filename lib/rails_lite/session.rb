ef591920f71424fbaefca84ce98029e1ebbf90d1
Eddie Salazar <eddie.salazar@gmail.com>
2014-04-30 14:04:34 -0700

    fished with session

--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -6,23 +6,24 @@ class Session
   # deserialize the cookie into a hash
   def initialize(req)
     cookie = req.cookies.select { |cook| cook.name == "_rails_lite_app"}.first
-    if cookie.value != {}
-      @cookie = cookie.value.to_json
-      puts @cookie
+    if cookie.nil?
+      @data = {}
     else
-      @cookie = {}
-      puts @cookie
+      @data = JSON.parse(cookie.value)
     end
   end
 
   def [](key)
+    @data[key]
   end
 
   def []=(key, val)
+    @data[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.cookies << WEBrick::Cookie.new("_rails_lite_app", @data.to_json)
   end
 end
